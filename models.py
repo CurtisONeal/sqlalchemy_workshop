483358dfdc745e8d57763fbadc5a26eb20922649
Curtis O <curtis@case.edu>
2023-07-15 15:40:48 -0400

    working

--- models.py
+++ models.py
@@ -35,12 +35,46 @@
 #  pip freeze > requirements.txt 
 
 
-from sqlalchemy import ( create_engine, Column, 
+from sqlalchemy import ( create_engine, Column, Integer,
                        Engine, String, ForeignKey)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+
 
 engine = create_engine("sqlite:///zoo.db", echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+
+# one class per table
+
+class Animal(Base):
+    __tablename__ = 'animals'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    habitat = Column(String)
+    logs = relationship("Logbook", back_populates="animals")
+
+    def __repr__(self):
+        return f"""
+    \nAnimal {self.id}\r 
+    Name = {self.name}\r 
+    Notes = {self.habitat}
+    """
+
+class Logbook(Base):
+    __tablename__ = 'logbook:'
+    
+    id = Column(Integer, primary_key=True)
+    animal_id = Column(Integer, ForeignKey("animals.id"))
+    notes = Column(String)
+    animal = relationship("Animal", back_populates='logs')
+    
+    def __repr__(self):
+        return f"""
+    \nLogbook {self.id}\r 
+    Animal ID = {self.animal_id}\r 
+    Notes = {self.notes}
+    """
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
