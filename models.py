5bc6767857dde7c8f951d3dbbce20fa3bddbb2ee
Curtis O <curtis@case.edu>
2023-07-15 07:03:57 -0400

    initial commit

---
+++ models.py
@@ -0,0 +1,27 @@
+# notes on the venv and start up
+# mac:
+#     python3 -m venv env
+#     source ./env/bin/activate
+    
+# Windows: python -m venv env
+# .\env\Scripts\activate
+
+# Both:
+#     pip install requests
+#     pip freeze > requirements.txt
+
+# reminders
+# git init 
+# git add .  
+# git commit -m 'Initial commit'
+# git branch -M main
+# git remote add origin https://github.com/CurtisONeal/<name>.git
+# git push -u origin main
+
+# reminder for second commit.... if you just tried git push
+# git push --set-upstream origin main
+# starting up the app reminder to test for typos:
+# python3 app.py # to see the db created
+
+# https://teamtreehouse.com/library/using-the-requests-library/using-the-requests-library
+# httpbin.org
